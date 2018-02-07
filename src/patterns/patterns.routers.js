e40b6d2ff1ae3a8da0ffb932f7c649ce5887d046
Adrian Bury <kubaadrian2@gmail.com>
2018-02-07 18:23:54 +0100

    Update router code

--- src/patterns/patterns.routers.js
+++ src/patterns/patterns.routers.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const PatternModel = require('./patterns.controllers');
 
 router
-    .get('/', function (req, res) {
-        PatternModel.getAll(req, res);
-    })
-    .post('/', function (req, res) {
-        PatternModel.addNew(req, res);
-    })
+    .get('/', PatternModel.getAll)
+
+    .post('/', PatternModel.addNew)
 ;
 
 module.exports = router;
