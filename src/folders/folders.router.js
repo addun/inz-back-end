fa8c45dca82343b6c827863bbb22b494ff5641b4
Adrian Bury <kubaadrian2@gmail.com>
2018-05-20 20:48:58 +0200

    Add "getFolderById" endpoint

--- src/folders/folders.router.js
+++ src/folders/folders.router.js
@@ -4,6 +4,7 @@ const foldersController = require('./folders.controller');
 
 router
     .get('/', foldersController.getFoldersTree)
+    .get('/:folderId', foldersController.getFolder)
     .post('/', foldersController.addFolder)
     .patch('/:folderId', foldersController.updateFolder)
     .delete('/:folderId', foldersController.removeFolder)
